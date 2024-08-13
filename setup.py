829f936292e2dfd0fa2d44ecd7de21f9ed2493a8
jhonkFr <jhonk.frz@gmail.com>
2024-08-12 22:40:16 -0500

    api

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DolarPeru",
+    version="0.1.1",
+    description="Scrapea el precio del dolar en diferentes casas de cambio y bancos",
+    author="Tjhon",
+    author_email="fr.jhonk@gmail.com",
+    packages=find_packages(),
+    install_requires=["selenium", "pandas", "numpy", "python-dotenv", "requests"],
+)
